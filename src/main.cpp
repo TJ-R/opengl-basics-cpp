be89b924ef572be5a59af33f2edeb59262b46ce2
TJ-R <runyant13@gmail.com>
2026-08-27 21:42:27 -0400

    Added camera.h file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,26 @@
+#include <glad/glad.h>
+
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+unsigned int loadTexture(const char *path);
+
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main() {
         GLFWwindow *window;
